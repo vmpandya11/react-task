50877132a0efc39f0c8ebccfff9fc12261ef7423
vmpandya11 <140773938+vmpandya11@users.noreply.github.com>
2024-02-03 20:48:11 +0530

    todotask

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
-
+import React, { useState } from 'react'
+import "./App.css"
+import TodoInput from './component/TodoInput'
+import Todolist from './component/TodoList';
 function App() {
+  const [listTodo, setListTodo] = useState([]);
+  let addList = (inputText) => {
+    if (inputText !== '')
+      setListTodo([...listTodo, inputText]);
+  }
+  const deleteListItem = (key) => {
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key, 1)
+    setListTodo([...newListTodo])
+  }
   return (
-    <div className="App">
-
+    <div className="main-container">
+      <div className="center-container">
+        <TodoInput addList={addList} />
+        <h1 className="app-heading">TODO</h1>
+        <hr />
+        {listTodo.map((listItem, i) => {
+          return (
+            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem} />
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
